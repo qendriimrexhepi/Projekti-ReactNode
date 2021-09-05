7ceac46b132fc79d2c0ea73daefab60039172a9f
qendriimrexhepi <qendriimrexhepi@gmail.com>
2021-09-05 23:25:29 +0200

    implement some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,8 @@ import LoadingBox from './components/LoadingBox';
 import MessageBox from './components/MessageBox';
 import MapScreen from './screens/MapScreen';
 import DashboardScreen from './screens/DashboardScreen';
+import SupportScreen from './screens/SupportScreen';
+import ChatBox from './components/ChatBox';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -134,6 +136,9 @@ function App() {
                   <li>
                     <Link to="/userlist">Users</Link>
                   </li>
+                  <li>
+                    <Link to="/support">Support</Link>
+                  </li>
                 </ul>
               </div>
             )}
@@ -235,6 +240,7 @@ function App() {
             path="/dashboard"
             component={DashboardScreen}
           ></AdminRoute>
+                    <AdminRoute path="/support" component={SupportScreen}></AdminRoute>
 
           <SellerRoute
             path="/productlist/seller"
@@ -248,10 +254,9 @@ function App() {
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">
-        < p className="col-sm">
-            &copy;{new Date().getFullYear()} ECOMMERCE | All rights reserved |
-            Terms Of Service | Privacy
-          </p>
+          {userInfo && !userInfo.isAdmin && <ChatBox userInfo={userInfo} />}
+          <div>&copy;{new Date().getFullYear()} ECOMMERCE | All rights reserved |
+            Terms Of Service | Privacy</div>{' '}
         </footer>
       </div>
     </BrowserRouter>
